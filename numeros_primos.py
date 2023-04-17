30abe4e0bd41a6811ecfebfd878f3016d2fc67a0
andre-min <andre.minato610@gmail.com>
2023-04-17 20:13:54 -0300

    subindo pro reposutorio

---
+++ numeros_primos.py
@@ -0,0 +1,31 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def nao_entre_em_panico():
+    qtdTotal = 100
+    primos = "1,2"
+    candPrimo = 3
+    qtdEncontrados = 2
+    ehPrimo = 1
+    numeros_primos = []
+
+    while qtdEncontrados < qtdTotal:
+        for i in range (2, candPrimo):
+            if candPrimo % i == 0:
+                ehPrimo = 0
+                break
+        if ehPrimo == 1:
+            primos = primos + str(candPrimo)+","
+            qtdEncontrados += 1
+            # if qtdEncontrados % 10 == 0:
+            #     primos += "<br>"
+        ehPrimo = 1
+        candPrimo += 1
+
+    numeros_primos.append(primos)
+    return numeros_primos
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=5000, debug=True)
